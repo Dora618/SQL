6be46d655e07e3c8a4b97b1eead5ba761ba3fe02
Dora618 <doradeng2022@outlook.com>
2025-06-13 05:13:10 +1000

    Create Data Clean & EDA for layoffs_v2.sql

---
+++ Data Clean & EDA for layoffs_v2.sql
@@ -0,0 +1,80 @@
+SELECT * FROM layoffs_v2.layoffs_v2;
+
+-- create staging table 
+
+create table layoffs_v2_staging
+like layoffs_v2;
+
+select *
+from layoffs_v2_staging;
+
+insert layoffs_v2_staging
+select *
+from layoffs_v2;
+
+
+-- check dusplicates
+
+with find_duplicate as 
+(
+	select *, row_number() 
+	over(partition by 
+			company, location, total_laid_off, 
+            `date`, percentage_laid_off, industry, 
+            'source', stage, funds_raised, country, date_added) as row_num
+	from layoffs_v2_staging
+) 
+select * from find_duplicate
+where row_num >1; 
+
+-- modify date column from text type to date type
+
+select `date`, 
+str_to_date(`date`, '%m/%d/%Y')
+from layoffs_v2_staging;
+
+update layoffs_v2_staging
+set 
+`date`= str_to_date(`date`, '%m/%d/%Y')
+;
+
+alter table layoffs_v2_staging
+modify column `date` date;
+
+
+-- delect rows where percentage_laid_off=0 and total_laid_off=0 
+
+delete
+from layoffs_v2_staging
+where percentage_laid_off=0 and total_laid_off=0;
+
+-- droup clumns that are not relevant 
+
+alter table layoffs_v2_staging
+drop column `source`,
+drop column date_added;
+
+-- only keep data after 2023-03-11
+
+delete 
+from layoffs_v2_staging
+where `date`<= '2023-03-11';
+
+-- check if there is any data clean needs
+
+select *
+from layoffs_v2_staging
+where industry is null or trim(industry)='';
+
+select *
+from layoffs_v2_staging
+where company='Appsmith';
+
+select distinct stage
+from layoffs_v2_staging
+order by 1;
+
+select company, sum(total_laid_off)
+from layoffs_v2_staging
+group by company
+order by 2;
